b088571027ec75f56d7b515ded623fc6266095d2
jchinchin <89807947+jchinchin@users.noreply.github.com>
2021-09-23 17:50:45 -0400

    Fixed Carousel

--- client/lab_4/script.js
+++ client/lab_4/script.js
@@ -1,44 +1,44 @@
-let slidePostion = 0;
-const slides = document.querySelectorAll('carousel_item')
-const totalSlides = slides.length;
+let picsPosition = 0;
+const pics = document.querySelectorAll(".carousel_item");
+const totalPics = pics.length;
 
-document.querySelector('carousel_button--next').addEventListener("click", function());
-{
-    updateSlidePostion(moveToNextSlide());
+document
+  .querySelector("#carousel_button--next")
+  .addEventListener("click", function () {
+    moveToNextPic();
+  });
+
+document
+  .querySelector("#carousel_button--prev")
+  .addEventListener("click", function () {
+    moveToPrevPic();
+  });
+
+function updatePicPosition() {
+  for (let pic of pics) {
+    pic.classList.remove("carousel_item--visible");
+    pic.classList.add("carousel_item--hidden");
+  }
+
+  pics[picsPosition].classList.add("carousel_item--visible");
 }
 
-document.querySelector('carousel_button--prev').addEventListener("click", function());
-{
-    updateSlidePostion(moveToPrevSlide());
+function moveToNextPic() {
+  if (picsPosition === totalPics - 1) {
+    picsPosition = 0;
+  } else {
+    picsPosition++;
+  }
+
+  updatePicPosition();
 }
 
-function updateSlidePostion() {
-    for (let slide of slides) {
-        slide.classList.remove('carousel_item--visible');
-        slide.classList.add('carousel_item--hidden');
-    }
+function moveToPrevPic() {
+  if (picsPosition === 0) {
+    picsPosition = totalPics - 1;
+  } else {
+    picsPosition--;
+  }
 
-    slides[slidePostion].classList.add('carousel_item--visible');
-}
-
-function moveToNextSlide() {
-    if(slidePostion == totalSlides -1) {
-        slidePostion=0;
-    }
-    else {
-        slidePostion++;
-    }
-
-    updateSlidePostion();
-}
-
-function moveToPrevSlide() {
-    if(slidePostion == 0) {
-        slidePostion = totalSlides - 1;
-    }
-    else {
-        slidePostion--;
-    }
-
-    updateSlidePostion();
+  updatePicPosition();
 }
