ebce58afb2c9a1b123fd6906a0f83c814f35ca7d
jchinchin <89807947+jchinchin@users.noreply.github.com>
2021-10-06 16:19:52 -0400

    Update script.js

--- client/tetris/script.js
+++ client/tetris/script.js
@@ -154,11 +154,11 @@ document.addEventListener('DOMContentLoaded', () => {
   const displayIndex = 0;
 
   const upNextTetrominoes = [
-    [1, displayWidth + 1, displayWidth * 2 + 1, 2], 
-    [0, displayWidth, displayWidth + 1, displayWidth * 2 + 1], 
-    [1, displayWidth, displayWidth + 1, displayWidth + 2], 
-    [0, 1, displayWidth, displayWidth + 1], 
-    [1, displayWidth + 1, displayWidth * 2 + 1, displayWidth * 3 + 1] 
+    [1, displayWidth + 1, displayWidth * 2 + 1, 2],
+    [0, displayWidth, displayWidth + 1, displayWidth * 2 + 1],
+    [1, displayWidth, displayWidth + 1, displayWidth + 2],
+    [0, 1, displayWidth, displayWidth + 1],
+    [1, displayWidth + 1, displayWidth * 2 + 1, displayWidth * 3 + 1]
   ];
 
   function displayShape() {
